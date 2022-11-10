dd93dd8d80fd8704b7e46563ffe030657d1874ac
S0vers <sovers.pandey@outlook.com>
2022-11-10 12:27:16 +0600

    banner complete

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import img from '../../../assets/banner/Banner.jpg'
+const Banner = () => {
+    return (
+        <div className="hero min-h-screen" style={{ backgroundImage: `url(${img})` }}>
+            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content text-center text-neutral-content">
+                <div className="max-w-md">
+                    <h1 className="mb-5 text-5xl font-bold">Hello there!!</h1>
+                    <p className="mb-5">Welcome to my website. I am a veteran content creator. I have been making content for more then two decades. Through the years of practice I have learned some ways to game the system. So what are you waiting for?</p>
+                    <button className="btn btn-primary">Get Started</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
 
     </>
     return (
-        <div className="navbar h-20 mb-12 bg-rose-50">
+        <div className="navbar h-20 bg-rose-50">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
