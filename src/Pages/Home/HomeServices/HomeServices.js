043b867d5e5385f28f183b3de5a537dbb18cbb0a
S0vers <sovers.pandey@outlook.com>
2022-11-10 15:33:13 +0600

    loader added

--- src/Pages/Home/HomeServices/HomeServices.js
+++ src/Pages/Home/HomeServices/HomeServices.js
@@ -4,22 +4,41 @@ import ServiceCard from '../../Shared/ServiceCard/ServiceCard';
 
 const HomeServices = () => {
     const [services, setSetServices] = useState([]);
+    const [loading, setLoading] = useState(true)
     useEffect(() => {
         fetch('http://localhost:5000/homeservices')
             .then(res => res.json())
-            .then(data => setSetServices(data))
+            .then(data => {
+                setSetServices(data)
+                setLoading(false)
+            })
     }, [])
+    const loadingItems = <>
+        {
+            loading ?
+                <>
+                    <div className='flex flex-col justify-center items-center'>
+                        <div className="radial-progress bg-red-500 animate-spin" style={{ "--value": 20 }}></div>
+                    </div>
+                </>
+                :
+                <>
+                    <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-5'>
+                        {
+                            services.map(service => <ServiceCard
+                                key={service._id}
+                                service={service}
+                            ></ServiceCard>)
+                        }
+                    </div>
+                </>
+
+        }
+    </>
     return (
         <div className='flex flex-col justify-center items-center '>
             <h1 className="my-5 text-5xl font-bold text-rose-600 text-center">My Services</h1>
-            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-5'>
-                {
-                    services.map(service => <ServiceCard
-                        key={service._id}
-                        service={service}
-                    ></ServiceCard>)
-                }
-            </div>
+            {loadingItems}
             <Link to='/services'><button className="btn btn-primary mt-20">See more</button></Link>
         </div>
     );
--- src/Pages/Services/Services/Services.js
+++ src/Pages/Services/Services/Services.js
@@ -1,21 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import ServiceCard from '../../Shared/ServiceCard/ServiceCard';
 
 const Services = () => {
-    const services = useLoaderData();
-    console.log(services);
+    const [services, setServices] = useState([]);
+    const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        fetch('http://localhost:5000/services')
+            .then(res => res.json())
+            .then(data => {
+                setServices(data)
+                setLoading(false)
+            })
+    }, [])
+    const loadingItems = <>
+        {
+            loading ?
+                <>
+                    <div className='flex flex-col justify-center items-center'>
+                        <div className="radial-progress bg-red-500 animate-spin" style={{ "--value": 20 }}></div>
+                    </div>
+                </>
+                :
+                <>
+                    <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-5'>
+                        {
+                            services.map(service => <ServiceCard
+                                key={service._id}
+                                service={service}
+                            ></ServiceCard>)
+                        }
+                    </div>
+                </>
+
+        }
+    </>
     return (
         <div className='flex mb-10 flex-col justify-center items-center '>
             <h1 className="my-5 text-5xl font-bold text-rose-600 text-center">All The Services</h1>
-            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-5'>
-                {
-                    services.map(service => <ServiceCard
-                        key={service._id}
-                        service={service}
-                    ></ServiceCard>)
-                }
-            </div>
+            {loadingItems}
         </div>
     );
 };
