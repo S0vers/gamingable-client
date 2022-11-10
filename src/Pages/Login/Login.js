5bc8f07e9aaf0540caa655a0c7190a5d2c8b28bc
S0vers <sovers.pandey@outlook.com>
2022-11-10 13:52:05 +0600

    Login layout complete

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,41 @@
 import React from 'react';
-
+import { FaGoogle, FaGithub } from "react-icons/fa";
 const Login = () => {
     return (
-        <div>
-            This is login.
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl  bg-rose-50">
+                    <div className="card-body">
+                        <h1 className="text-5xl font-bold">Login now!</h1>
+                        <form >
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input name='password' type="text" placeholder="email" className="input input-bordered " required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input name='password' type="password" placeholder="password" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Login</button>
+                            </div>
+                        </form>
+
+                    </div>
+                    <div className='text-center mb-5'>
+                        <button className="btn text-lg btn-circle mx-2">
+                            <FaGoogle></FaGoogle>
+                        </button>
+                        <button className="btn text-lg btn-circle mx-2">
+                            <FaGithub></FaGithub>
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
